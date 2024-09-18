1e4227e6234a7b7281d4a6ac057e0fe148fc9a69
akmalhossain <akmalforu@gmail.com>
2024-09-18 20:01:40 +0600

    login form added

--- js/login.js
+++ js/login.js
@@ -6,5 +6,16 @@ document.getElementById('button-login')
         event.preventDefault();
         // step 3 get the phone number
         const phoneNumber = document.getElementById('phone-no').value;
-        console.log(phoneNumber);
+        const pinNumber =document.getElementById('pin-no').value;
+        console.log(phoneNumber, pinNumber);
+        // this is temporaty
+        // step 4 validation of phone num and pin
+        if(phoneNumber === '5' && pinNumber === '1234'){
+            //redirect home page
+            window.location.href = './home/home.html'
+        }
+        else{
+            alert('wrong phone number or pin')
+        }
+
     })
